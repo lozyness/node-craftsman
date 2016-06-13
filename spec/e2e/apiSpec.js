678f4a42270d21f79e31f1cb614760d7610abb49
Lozyness <lozyness@gmail.com>
2016-06-13 23:13:51 +0100

    Updating tests and adding travis config

--- spec/e2e/apiSpec.js
+++ spec/e2e/apiSpec.js
@@ -1,3 +1,20 @@
-/**
- * Created by laura on 13/06/2016.
- */
+'use strict';
+
+var request = require('request');
+
+describe ('The API', function() {
+
+    it ('should respond to a GET request at /api/keywords/', function (done) {
+        request.get(
+            {
+                'url': 'http://localhost:8080/api/keywords/',
+                'json': true
+            },
+            function (err, res, body) {
+                expect(res.statusCode).toBe(200);
+                expect(body.foo).toEqual('bar');
+                done();
+            }
+        )
+    });
+});
